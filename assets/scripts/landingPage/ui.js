fb536aa486d577917295f1cb46b5f52045b5ec70
kailinc <kailinc@bu.edu>
2017-07-20 22:35:05 -0400

    Started function to scroll to page section

--- assets/scripts/landingPage/ui.js
+++ assets/scripts/landingPage/ui.js
@@ -1,1 +1,13 @@
-
+// 'use strict'
+//
+// const clickScroll = functon (event) {
+//   event.preventDefault()
+//
+//   let $anchor = $(this)
+//   $('html, body').stop().animate({
+//     scrollTop: $($anchor.attr('href')).offset().top
+//   }, 1500, 'easeInOutExpo')
+//
+// }
+//
+// $('.page-scroll a').bind('click', clickScroll)
